f0014340d7ad8bdb0f8a73543b58b9afde231f45
eitay <eitay@soos.org.il>
2023-04-12 00:48:13 +0300

    semi-working search

--- BirdsControl/BirdsControl/SearchBird.xaml.cs
+++ BirdsControl/BirdsControl/SearchBird.xaml.cs
@@ -12,20 +12,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace BirdsControl {
+namespace BirdsControl
+{
     /// <summary>
     /// Interaction logic for SearchBird.xaml
     /// </summary>
-    public partial class SearchBird : Window {
-        public SearchBird() {
+    public partial class SearchBird : Window
+    {
+        public SearchBird()
+        {
             InitializeComponent();
         }
 
-        private void Search_bird_Click(object sender, RoutedEventArgs e) {
+        private void Search_bird_Click(object sender, RoutedEventArgs e)
+        {
+            var query = "SELECT * From Bird";
+            var specie = this.species_tb.Text;
+            var sub = this.subspecies_tb.Text;
+            var date = this.datePicker.Text;
+            var sex = this.sex_tb.Text;
+            var cage = this.cage_tb.Text;
 
+            if (specie != "")
+            {
+                query += " WHERE Specie = '" + specie + "' ";
+            }
+
+            if (sub != "")
+            {
+                query += " WHERE SubSpice = '" + sub + "' ";
+            }
+
+            if (date != "")
+            {
+                query += " WHERE HatchingDay = '" + date + "' ";
+            }
+
+            if (sex != "")
+            {
+                query += " WHERE Sex = '" + sex + "' ";
+            }
+
+            if (cage != "")
+            {
+                query += " WHERE Cage = '" + cage + "' ";
+            }
+
+            using (BirdsControlDBEntities db = new BirdsControlDBEntities())
+            {
+                Console.WriteLine(query);
+                var results = db.Bird.SqlQuery(query).ToList();
+                this.gridBird.ItemsSource = results.ToList();
+            }
         }
 
-        private void HomePage_btn_Click(object sender, RoutedEventArgs e) {
+        private void HomePage_btn_Click(object sender, RoutedEventArgs e)
+        {
             MainPage newWindow = new MainPage();
             this.Visibility = Visibility.Hidden;
             newWindow.Show();
