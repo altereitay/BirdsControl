45bd78d2745897c238c6a7246fc2931748a67ca8
altereitay <alter1eitai@gmail.com>
2023-04-12 11:32:05 +0300

    almost fully working, date still not working

--- BirdsControl/BirdsControl/SearchBird.xaml.cs
+++ BirdsControl/BirdsControl/SearchBird.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace BirdsControl
             InitializeComponent();
         }
 
-        private void Search_bird_Click(object sender, RoutedEventArgs e)
+        /*private void Search_bird_Click(object sender, RoutedEventArgs e)
         {
             var query = "SELECT * From Bird";
             var specie = this.species_tb.Text;
@@ -32,39 +33,169 @@ namespace BirdsControl
             var date = this.datePicker.Text;
             var sex = this.sex_tb.Text;
             var cage = this.cage_tb.Text;
+            var whereClause = "";
 
-            if (specie != "")
+            if (!string.IsNullOrEmpty(specie))
             {
-                query += " WHERE Specie = '" + specie + "' ";
+                whereClause += "WHERE Specie = '" + specie + "'";
             }
 
-            if (sub != "")
+            if (!string.IsNullOrEmpty(sub))
             {
-                query += " WHERE SubSpice = '" + sub + "' ";
+                if (string.IsNullOrEmpty(whereClause))
+                {
+                    whereClause += "WHERE ";
+                }
+                else
+                {
+                    whereClause += " AND ";
+                }
+                whereClause += "SubSpice = '" + sub + "'";
+            }
+            
+            if (!string.IsNullOrEmpty(date))
+            {
+                if (string.IsNullOrEmpty(whereClause))
+                {
+                    whereClause += "WHERE ";
+                }
+                else
+                {
+                    whereClause += " AND ";
+                }
+                whereClause += "HatchingDay = '" + date + "'";
+            }
+            
+            if (!string.IsNullOrEmpty(sex))
+            {
+                if (string.IsNullOrEmpty(whereClause))
+                {
+                    whereClause += "WHERE ";
+                }
+                else
+                {
+                    whereClause += " AND ";
+                }
+                whereClause += "Sex = '" + sex + "'";
+            }
+            
+            if (!string.IsNullOrEmpty(cage))
+            {
+                if (string.IsNullOrEmpty(whereClause))
+                {
+                    whereClause += "WHERE ";
+                }
+                else
+                {
+                    whereClause += " AND ";
+                }
+                whereClause += "Cage = '" + cage + "'";
+            }
+            
+            if (!string.IsNullOrEmpty(whereClause))
+            {
+                query += " " + whereClause;
             }
 
-            if (date != "")
-            {
-                query += " WHERE HatchingDay = '" + date + "' ";
-            }
-
-            if (sex != "")
-            {
-                query += " WHERE Sex = '" + sex + "' ";
-            }
-
-            if (cage != "")
-            {
-                query += " WHERE Cage = '" + cage + "' ";
-            }
 
             using (BirdsControlDBEntities db = new BirdsControlDBEntities())
             {
-                Console.WriteLine(query);
+                // Console.WriteLine(query);
                 var results = db.Bird.SqlQuery(query).ToList();
                 this.gridBird.ItemsSource = results.ToList();
             }
+        }*/
+        private void Search_bird_Click(object sender, RoutedEventArgs e)
+{
+    var query = "SELECT * FROM Bird";
+    var specie = SanitizeInput(this.species_tb.Text);
+    var sub = SanitizeInput(this.subspecies_tb.Text);
+    var date = SanitizeInput(this.datePicker.Text);
+    var sex = SanitizeInput(this.sex_tb.Text);
+    var cage = SanitizeInput(this.cage_tb.Text);
+    var whereClause = "";
+
+    if (!string.IsNullOrEmpty(specie))
+    {
+        whereClause += "WHERE Specie = @specie";
+    }
+
+    if (!string.IsNullOrEmpty(sub))
+    {
+        if (string.IsNullOrEmpty(whereClause))
+        {
+            whereClause += "WHERE ";
         }
+        else
+        {
+            whereClause += " AND ";
+        }
+        whereClause += "SubSpecie = @sub";
+    }
+
+    if (!string.IsNullOrEmpty(date))
+    {
+        if (string.IsNullOrEmpty(whereClause))
+        {
+            whereClause += "WHERE ";
+        }
+        else
+        {
+            whereClause += " AND ";
+        }
+        whereClause += "HatchingDate = @date";
+    }
+
+    if (!string.IsNullOrEmpty(sex))
+    {
+        if (string.IsNullOrEmpty(whereClause))
+        {
+            whereClause += "WHERE ";
+        }
+        else
+        {
+            whereClause += " AND ";
+        }
+        whereClause += "Sex = @sex";
+    }
+
+    if (!string.IsNullOrEmpty(cage))
+    {
+        if (string.IsNullOrEmpty(whereClause))
+        {
+            whereClause += "WHERE ";
+        }
+        else
+        {
+            whereClause += " AND ";
+        }
+        whereClause += "CageNumber = @cage";
+    }
+
+    if (!string.IsNullOrEmpty(whereClause))
+    {
+        query += " " + whereClause;
+    }
+
+    using (BirdsControlDBEntities db = new BirdsControlDBEntities())
+    {
+        var results = db.Bird.SqlQuery(query,
+            new SqlParameter("@specie", specie),
+            new SqlParameter("@sub", sub),
+            new SqlParameter("@date", date),
+            new SqlParameter("@sex", sex),
+            new SqlParameter("@cage", cage)).ToList();
+        Console.WriteLine(results);
+        this.gridBird.ItemsSource = results.ToList();
+    }
+}
+
+private string SanitizeInput(string input)
+{
+    return input.Replace("'", "''");
+}
+
+
 
         private void HomePage_btn_Click(object sender, RoutedEventArgs e)
         {
